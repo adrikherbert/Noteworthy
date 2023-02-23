3234f90f0fe5353adee0d144b1750a4d79dc411e
QuinHouck <qhouck@purdue.edu>
2023-02-22 20:04:56 -0500

    basic note displaying and padding css changes

---
+++ frontend/src/components/Notes.js
@@ -0,0 +1,46 @@
+import { Outlet, Link } from "react-router-dom";
+import { useEffect, useState } from 'react';
+
+import Note from './Note.js'
+
+import './notes.css';
+
+const Notes = ({user_id}) => {
+    const [notes, setNotes] = useState([]);
+
+
+    useEffect(() => {
+        getNotes();
+    }, [])
+
+    async function getNotes() {
+        let list = [];
+
+        for (let i=0; i<10; i++){
+            const data = {
+                id: i,
+                title: "This is a note title",
+                text: "This is some text for a random note. It is slightlty longer. Actually a lot longer as people can type a shit ton for this part. hhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh",
+            }
+            list.push(data);
+        }
+        setNotes(list);
+    }
+
+    return(
+        <div className="notelist_container">
+            <div className="notelist_title">
+                <h1>Notes - {user_id}</h1>
+            </div>
+            <div className="notelist">
+                <ul>
+                    {notes.map((data) => 
+                        <Note data={data}/>
+                    )}
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Notes
