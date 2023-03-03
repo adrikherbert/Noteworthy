4c64a7cc16ed4784467d07c6f987ab38c496040c
joshkleyman <jkleyman@purdue.edu>
2023-03-03 14:36:38 -0500

    add mail env to actual config

--- backend/server/config.py
+++ backend/server/config.py
@@ -32,3 +32,12 @@ JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=2)
 CORS_HEADERS='Content-Type'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# Mail
+MAIL_SERVER=os.environ.get("MAIL_SERVER")
+MAIL_PORT=os.environ.get("MAIL_PORT")
+MAIL_USERNAME=os.environ.get("MAIL_USERNAME")
+MAIL_PASSWORD=os.environ.get("MAIL_PASSWORD")
+MAIL_USE_TLS=os.environ.get("MAIL_USE_TLS")
+MAIL_USE_SSL=os.environ.get("MAIL_USE_SSL")
+MAIL_DEFAULT_SENDER=os.environ.get("MAIL_DEFAULT_SENDER")
